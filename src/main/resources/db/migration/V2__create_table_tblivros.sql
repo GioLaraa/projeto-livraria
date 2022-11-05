3377fe463eb0e89d3c77a2e0e889ea9558f8b6d4
Giovanni <80859834+Giovanni-SVC@users.noreply.github.com>
2022-11-05 12:21:18 -0300

    removendo tabela duplicada

--- src/main/resources/db/migration/V2__create_table_tblivros.sql
+++ src/main/resources/db/migration/V2__create_table_tblivros.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `tblivros` (
-  `codlivro` int(5) NOT NULL AUTO_INCREMENT,
+  `codlivro` int(5) NOT NULL,
   `titulo` varchar(255) DEFAULT NULL,
   `autor` varchar(150) DEFAULT NULL,
   `categoria` varchar(40) DEFAULT NULL,

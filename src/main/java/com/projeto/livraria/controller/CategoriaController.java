e0784360bc18fa9d91d3534e48cebe3153ad7ebb
Marcos Augusto Quiuqui <74932459+Mquiuqui@users.noreply.github.com>
2022-11-04 23:34:00 -0300

    criação de classes controller

---
+++ src/main/java/com/projeto/livraria/controller/CategoriaController.java
@@ -0,0 +1,12 @@
+package com.projeto.livraria.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("categorias")
+public class CategoriaController {
+
+}
+	
--- src/main/java/com/projeto/livraria/controller/HomeController.java
+++ src/main/java/com/projeto/livraria/controller/HomeController.java
@@ -1,14 +1,33 @@
 package com.projeto.livraria.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.projeto.livraria.model.Livros;
+import com.projeto.livraria.repository.LivrosRepository;
 
 @Controller
 public class HomeController {
 	
-	@GetMapping(value = "/")
-	public String homePage() {
-		return "index";
-	}
-
+//	@GetMapping(value = "/")
+//	public String homePage() {
+//		return "index";
+//	}
+	
+	@Autowired
+	private LivrosRepository livrosRepository;
+	
+	
+	@GetMapping("/home")
+	public ModelAndView home() {
+	    List<Livros> livros = livrosRepository.findAll();
+	    ModelAndView mv = new ModelAndView("index");
+	    mv.addObject("categorias", livros);
+	    return mv; 
+	}   
+	
 }
